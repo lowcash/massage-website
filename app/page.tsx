d83138f9489c4c6cc887efc3196ad2a4a79cfd14
lukas <machala.lukas@post.cz>
2025-05-19 01:04:01 +0200

    chore: temporarily hides calendar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
-import { getCalendar } from '@/app/actions/calendar'
+// import { getCalendar } from '@/app/actions/calendar'
 
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Services from '@/components/Services'
-import Calendar from '@/components/Calendar'
+// import Calendar from '@/components/Calendar'
 import About from '@/components/About'
 import Contact from '@/components/Contact'
 import Footer from '@/components/Footer'
 
 export default async function Page() {
-  const calendarData = (await getCalendar())?.data
+  // const calendarData = (await getCalendar())?.data
 
   return (
     <>
       <Header />
       <Hero />
       <Services />
-      {calendarData && <Calendar data={calendarData} />}
+      {/* {calendarData && <Calendar data={calendarData} />} */}
       <About />
       <Contact />
       <Footer />
