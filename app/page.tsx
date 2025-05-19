d9c4793a10c63a8161229f67d1c3803bdd3b4875
lukas <machala.lukas@post.cz>
2025-05-20 00:45:22 +0200

    fix: attempt calendar revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,14 @@ export const revalidate = 60
 export default async function Page() {
   const calendarData = (await getCalendar())?.data
 
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+  
   return (
     <>
+      {today.getTime()}
+      <br />
+      {calendarData?.[0].date.getTime()}
       <Header />
       <Hero />
       <Services />
