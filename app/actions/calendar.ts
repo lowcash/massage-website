22e98ad0e175b1e6fe928c8408d9db0f68bc2198
lukas <machala.lukas@post.cz>
2025-05-19 14:25:25 +0200

    fix: attempt to fix data store

--- app/actions/calendar.ts
+++ app/actions/calendar.ts
@@ -6,10 +6,16 @@ import { calendarUpdateInputSchema } from '@/app/actions/calendar.schema'
 
 const CALENDAR_KV_KEY = 'calendar'
 
-const redis = await createClient({ url: process.env.REDIS_URL }).connect()
+async function getRedisClient() {
+  const client = createClient({ url: process.env.REDIS_URL })
+  await client.connect()
+  return client
+}
 
 export const getCalendar = actionClient.action(async () => {
+  const redis = await getRedisClient()
   const fileContent = await redis.get(CALENDAR_KV_KEY)
+  await redis.disconnect()
   if (!fileContent) {
     return []
   }
@@ -27,5 +33,7 @@ export const getCalendar = actionClient.action(async () => {
 })
 
 export const updateCalendar = authActionClient.schema(calendarUpdateInputSchema).action(async ({ parsedInput }) => {
+  const redis = await getRedisClient()
   await redis.set(CALENDAR_KV_KEY, JSON.stringify(parsedInput, null, 2))
+  await redis.disconnect()
 })
