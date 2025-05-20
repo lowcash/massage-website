0f1d62e19601bb3746ce75ef3443bcd8282bde54
lukas <machala.lukas@post.cz>
2025-05-20 02:09:52 +0200

    fix: attempt calendar ssr date

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -28,22 +28,16 @@ export default function Calendar(p: Props) {
   const scrollToContact = useScrollToElement()
   const visibleDays = useVisibleDays()
 
-  // const isClient = useIsClient()
-
-  // const data = useMemo(() => {
-  //   if (!isClient) return []
-
-  //   const today = new Date()
-  //   today.setHours(0, 0, 0, 0)
-
-  //   return p.data.filter((x) => x.date > today)
-  // }, [isClient, p.data])
+  const isClient = useIsClient()
 
   const data = useMemo(() => {
-    const now = new Date()
-    const todayUTC = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()))
-    return p.data.filter((x) => x.date > todayUTC)
-  }, [p.data])
+    if (!isClient) return []
+
+    const today = new Date()
+    today.setHours(0, 0, 0, 0)
+
+    return p.data.filter((x) => x.date > today)
+  }, [isClient, p.data])
 
   // Skupiny slotů podle dne
   const groupedDays = groupSlotsByDay(data)
