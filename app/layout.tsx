b08fb4e7c925601acccd7baf1b3067041ca43fff
lukas <machala.lukas@post.cz>
2025-11-16 18:03:03 +0700

    fix: update Google Analytics tracking ID to use environment variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,13 +28,13 @@ export default function RootLayout({
           {children}
         </BookingProvider>
 
-        <Script src='https://www.googletagmanager.com/gtag/js?id=G-BJWHB0F4BE' strategy='afterInteractive' />
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`} strategy='afterInteractive' />
         <Script id='gtag-init' strategy='afterInteractive'>
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-BJWHB0F4BE');
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
           `}
         </Script>
       </body>
